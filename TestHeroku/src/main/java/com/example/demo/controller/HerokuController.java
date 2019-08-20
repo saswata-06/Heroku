50f89a81b5a555d6536fd22eaecc6709870414fa
AE205_PC9 <AE205_PC9@Infva06879>
2019-08-20 15:23:43 -0700

    1st Push

---
+++ TestHeroku/src/main/java/com/example/demo/controller/HerokuController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+	public class HerokuController {
+
+		@GetMapping("/hello")
+		public String sayHello()
+		{
+			return "Hello World!!";
+		}
+	}
+
+
